24e1645b6f8022176be73cf6b12f9311c32a6a2d
andres-martin <andresmartin266@gmail.com>
2019-08-22 22:23:31 -0500

    update and destroy endpoints done

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ProductsController < ApplicationController
+  # before_filter :find_product, only: [:update, :destroy]
   include Response
-
+  
   def index
     @products = Product.all
     render json: @products
@@ -24,12 +25,27 @@ class Api::V1::ProductsController < ApplicationController
   end
 
   def update
-    @product = Product.update(params[:id], product_params)
+    product = Product.find(params[:id])
+    if product.update(product_params)
+      json_response(product, 200)      
+    else
+      json_response(product.errors, 422)
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render status: 204
   end
 
   private
     def product_params
       params.require(:product).permit(:name, :price)
       # params.permit(:name, :price)
+    end
+
+    def find_product
+      @product = Product.find(params[:id])
     end
 end
